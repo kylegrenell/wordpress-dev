4e0208a8afa6727b9a892003d93b749695dfd1a8
Kyle Grenell <kyle.grenell@gmail.com>
2017-12-11 13:35:39 +0000

    functions and variables basics

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,9 @@
-This is a custom theme by KG
+<?php 
+  function greet($name, $colour){
+    echo "<p>Hi my name is $name and my favourite colour is $colour</p>";
+  };
+
+  greet("John", "blue");
+  greet("Jane", "green");
+
+?> 
