3c2f5683d87fecdd244ea592dbc76187f248a280
Kyle Grenell <kyle.grenell@gmail.com>
2017-12-22 14:04:45 +0000

    filling out content from exisiting html w php

--- app/sql/local.wp_postmeta.sql
+++ app/sql/local.wp_postmeta.sql
@@ -2,4 +2,10 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_postmeta` VALUES
-(1,2,"_wp_page_template","default");
+(1,2,"_wp_page_template","default"),
+(2,4,"_edit_last","1"),
+(3,4,"_edit_lock","1513002607:1"),
+(6,6,"_edit_last","1"),
+(7,6,"_edit_lock","1513002635:1"),
+(10,8,"_edit_last","1"),
+(11,8,"_edit_lock","1513013223:1");
