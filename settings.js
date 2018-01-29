6a2fcd5eae8ab9c3dcd0dc8ec4807179020dbf32
Kyle Grenell <kyle.grenell@gmail.com>
2018-01-29 13:25:35 +0000

    gitignore added

--- settings.js
+++ settings.js
@@ -1,2 +1,2 @@
 exports.themeLocation = './app/wp-content/themes/fictional-university-theme/';
-exports.urlToPreview = 'http://fictional-university.test';
+exports.urlToPreview = 'http://fictional-university.local';
